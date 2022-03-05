9e91be8257e740e380693f5ffc1d5670c20b6a52
Sejal <Sejal>
2022-03-05 22:42:40 +0530

    functions and intro of JS

---
+++ function.js
@@ -0,0 +1,25 @@
+function sayHello(){
+    console.log("Hello to funtion")
+}
+sayHello();
+
+function sum(num1, num2)
+{
+    let add  = num1+num2;
+    console.log("Addition of given numbers: "+ add);
+}
+sum(7,3);
+
+function mul(num1, num2)
+{
+    return num1*num2;
+}
+let ans=mul(3,5);
+console.log(ans);
+
+
+let a = function sub(num1, num2){
+    return num1-num2;
+}
+console.log(a(10,5));
+
