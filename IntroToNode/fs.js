30a314a08d5ddcaae7720c810e6160259aa16eea
Sejal <Sejal>
2022-03-20 18:03:55 +0530

    directory CRUD done

--- IntroToNode/fs.js
+++ IntroToNode/fs.js
@@ -1,10 +1,46 @@
 let fs  = require("fs");
-console.log(fs);
+// console.log(fs);
 
 let path = require("path");
-let filePath = path.join(__dirname, "file.txt");
-console.log(filePath);
+// let filePath = path.join(__dirname, "file.txt");
+// console.log(filePath);
 
-fs.writeFileSync(filePath, "Hello!!! I am new text file");
+// //C-create
+// //creates file if doesn't exist, else overwrites existing file
+// fs.writeFileSync(filePath, "Hello!!! I am new text file");
+
+// //R-read
+// console.log("Before Update: \n");
+// let content = fs.readFileSync(filePath, 'utf-8');
+// console.log(content);
+
+// //U-update
+// fs.appendFileSync(filePath, "\nNewly added content");
+// console.log("\nAfter Update : \n");
+// console.log(fs.readFileSync(filePath, 'utf-8'));
+
+// //D-delete
+// // fs.unlinkSync(filePath);
+
+//Create a  Directory
+
+if(!fs.existsSync("hamariDirectory"));
+    fs.mkdirSync("hamariDirectory");
+
+//Read  a  Directory
+
+let folderPath = __dirname;
+
+let contentOfFolder = fs.readdirSync(folderPath);
+console.log(contentOfFolder);
+
+//Delete a Directory
+fs.rmdirSync("hamariDirectory");
+
+
+
+
+
+
 
 
---
+++ IntroToNode/module/testmodule.js
@@ -0,0 +1,6 @@
+let calc = require("./calculator.js");
+console.log(calc);
+
+console.log(calc.addition(3,6));
+console.log(calc.multiply(6,9));
+console.log(calc.division(65,5));
